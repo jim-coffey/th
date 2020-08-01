371ad9bf1dc16174b77cbe43edda22d39e181729
lazee <jim_coffey@hotmail.com>
2020-08-01 15:48:10 +0100

    added simple graphql schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,11 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
-
-app.use(bodyParser.json());
-
-app.get('/listings', (_req, res) => res.send(listings));
-
-app.post('/delete', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.status(404).send('Listing not found');
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
