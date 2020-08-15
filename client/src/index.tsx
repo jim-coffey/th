7c12fb7ae34b6f3f5b37be8f3023364bc5329db3
lazee <jim_coffey@hotmail.com>
2020-08-15 18:35:55 +0100

    google

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
-import { Home, Host, Listing, Listings, User, NotFound } from './sections';
+import { Layout } from 'antd';
+import {
+  Home,
+  Host,
+  Listing,
+  Listings,
+  Login,
+  User,
+  NotFound,
+} from './sections';
+import { Viewer } from './lib/types';
 import './styles/index.css';
 
 const client = new ApolloClient({
   uri: '/api',
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route
+            exact
+            path="/login"
+            render={props => <Login {...props} setViewer={setViewer} />}
+          />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
---
+++ client/src/sections/Login/index.tsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import { Card, Layout, Spin, Typography } from 'antd';
+import { ErrorBanner } from '../../lib/components';
+import { AUTH_URL } from '../../lib/graphql/queries';
+import { LOG_IN } from '../../lib/graphql/mutations';
+import { AuthUrl as AuthUrlData } from '../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl';
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from '../../lib/graphql/mutations/Login/__generated__/LogIn';
+import {
+  displaySuccessNotification,
+  displayErrorMessage,
+} from '../../lib/utils';
+import { Viewer } from '../../lib/types';
+
+import googleLogo from './assets/google_logo.jpg';
+
+const { Content } = Layout;
+const { Text, Title } = Typography;
+
+interface Props {
+  setViewer: (viewer: Viewer) => void;
+}
+
+export const Login = (props: Props) => {
+  const { setViewer } = props;
+  const client = useApolloClient();
+  const [
+    logIn,
+    { data: logInData, loading: logInLoading, error: logInError },
+  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: data => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+        displaySuccessNotification('You`ve successfully logged in!');
+      }
+    },
+  });
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: { code },
+        },
+      });
+    }
+  }, []);
+
+  const handleAuth = async () => {
+    try {
+      const { data } = await client.query<AuthUrlData>({
+        query: AUTH_URL,
+      });
+
+      window.location.href = data.authUrl;
+    } catch (err) {
+      displayErrorMessage(
+        'Sorry! We weren`t able to log you in. Please try again later!'
+      );
+    }
+  };
+
+  if (logInLoading) {
+    return (
+      <Content className="log-in">
+        <Spin size="large" tip="Logging In"></Spin>
+      </Content>
+    );
+  }
+
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+
+    return <Redirect to={`/user/${viewerId}`} />;
+  }
+
+  const logInErrorBannerElement = logInError ? (
+    <ErrorBanner description="Sorry! We weren`t able to log you in. Please try again later!" />
+  ) : null;
+
+  return (
+    <Content className="log-in">
+      {logInErrorBannerElement}
+      <Card className="log-in-card">
+        <div className="log-in-card__intro">
+          <Title level={3} className="log-in-card__intro-title">
+            <span role="img" aria-label="wave">
+              👋
+            </span>
+          </Title>
+          <Title level={3} className="log-in-card__intro-title">
+            Log in to TinyHouse!
+          </Title>
+          <Text>Sign in with Google to start booking available rentals!</Text>
+        </div>
+
+        <button className="log-in-card__google-button" onClick={handleAuth}>
+          <img
+            src={googleLogo}
+            alt="Google Logo"
+            className="log-in-card__google-button-logo"
+          />
+          <span className="log-in-card__google-button-text">
+            Sign in with Google
+          </span>
+        </button>
+
+        <Text type="secondary">
+          Note: By signing in, you'll be redirectoed to Google consent form to
+          sign in with your Google account.
+        </Text>
+      </Card>
+    </Content>
+  );
+};
