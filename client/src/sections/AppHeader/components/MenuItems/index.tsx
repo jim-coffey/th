420f49f7c5461abb85eb6d61515e88d180dc7d98
lazee <jim_coffey@hotmail.com>
2020-08-15 18:35:55 +0100

    google oauth 2

---
+++ client/src/sections/AppHeader/components/MenuItems/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
+import { Avatar, Button, Menu } from 'antd';
+import { HomeOutlined, UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import { LOG_OUT } from '../../../../lib/graphql/mutations';
+import { LogOut as LogOutData } from '../../../../lib/graphql/mutations/Logout/__generated__/LogOut';
+import {
+  displaySuccessNotification,
+  displayErrorMessage,
+} from '../../../../lib/utils';
+import { Viewer } from '../../../../lib/types';
+
+const { Item, SubMenu } = Menu;
+
+interface Props {
+  viewer: Viewer;
+  setViewer: (viewer: Viewer) => void;
+}
+
+export const MenuItems = ({ viewer, setViewer }: Props) => {
+  const [logOut] = useMutation<LogOutData>(LOG_OUT, {
+    onCompleted: data => {
+      if (data.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification('You`ve successfully logged out!');
+      }
+    },
+    onError: err => {
+      displayErrorMessage(
+        'Sorry! We weren`t able to log you out. Please try again.'
+      );
+    },
+  });
+
+  const handleLogOut = () => {
+    logOut();
+  };
+
+  const subMenuLogin =
+    viewer.id && viewer.avatar ? (
+      <SubMenu title={<Avatar src={viewer.avatar} />}>
+        <Item key="/user">
+          <Link to="/user">
+            <UserOutlined />
+            Profile
+          </Link>
+        </Item>
+        <Item key="/logout">
+          <div onClick={handleLogOut}>
+            <LogoutOutlined />
+            Log Out
+          </div>
+        </Item>
+      </SubMenu>
+    ) : (
+      <Item>
+        <Link to="/login">
+          <Button type="primary">Sign In</Button>
+        </Link>
+      </Item>
+    );
+
+  return (
+    <Menu mode="horizontal" selectable={false} className="menu">
+      <Item key="/host">
+        <Link to="/host">
+          <HomeOutlined type="home" />
+          Host
+        </Link>
+      </Item>
+      {subMenuLogin}
+    </Menu>
+  );
+};
