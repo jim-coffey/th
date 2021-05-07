54ec684116c1d53f6f58330b2fc21ed89119ae08
lazee <jim_coffey@hotmail.com>
2021-05-07 14:00:49 +0100

    home page query price sorted listings

---
+++ client/src/lib/graphql/queries/Listings/index.ts
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-boost';
+
+export const LISTINGS = gql`
+  query Listings($filter: ListingsFilter!, $limit: Int!, $page: Int!) {
+    listings(filter: $filter, limit: $limit, page: $page) {
+      total
+      result {
+        id
+        title
+        description
+        image
+        address
+        price
+        numOfGuests
+      }
+    }
+  }
+`;
